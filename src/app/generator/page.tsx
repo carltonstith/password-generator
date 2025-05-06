1b4cdc7b2be2cd7c7f7bf22241920812491546c5
Carlton Stith <stith@temple.edu>
2025-05-06 10:42:06 -0400

    removed import

--- src/app/generator/page.tsx
+++ src/app/generator/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import RangeSlider from "../rangeslider/page";
 import Footer from "../footer/page";
 
 const allOptions = [
@@ -188,13 +187,6 @@ export default function Generator() {
                     </span>
                   </button>
                 </div>
-                {/* <div onClick={() => {
-                navigator.clipboard.writeText(password);
-                alert("Password copied to clipboard!");
-              }}>
-              <span>{password}</span>
-              <button type="button" aria-label="Copy to clipboard" className="absolute right-2 top-2 p-1 sm:p-2 rounded-md hover:bg-gray-800 transition-colors"><span className="flex items-center justify-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"></rect><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"></path></svg></span></button>
-            </div> */}
               </div>
             </form>
             {/* <div className="mt-10">
